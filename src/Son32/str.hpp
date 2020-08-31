0d957cafc4e73fee3a915454c23bd03787af5ad4
kvk1920 <kvk1901@yandex.ru>
2020-08-31 19:06:56 +0300

    fix cyrillic in channel names etc

--- src/Son32/str.hpp
+++ src/Son32/str.hpp
@@ -6,23 +6,7 @@
 namespace Son32
 {
 
-[[nodiscard]] inline
+[[nodiscard]]
 std::string wstringToString(const std::wstring& wstr);
 
-}
-
-[[nodiscard]] std::string
-Son32::wstringToString(const std::wstring& wstr)
-{
-    std::string str;
-    for (auto c : wstr)
-    {
-        if (static_cast<int>(c) > 127)
-            str.push_back('_');
-        else
-            str.push_back(c);
-        if (isblank(str.back()))
-            str.back() = '_';
-    }
-    return str;
 }
