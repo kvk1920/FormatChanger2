d5a3e548c28c1eed1962aff4d9f7c526ec4f8f83
kvk1920 <kvk1901@yandex.ru>
2020-09-02 00:21:47 +0300

    TeeBuf

---
+++ src/utils/streams/tee.cpp
@@ -0,0 +1,42 @@
+#include <utils/streams/tee.hpp>
+
+namespace kvk1920::utils
+{
+
+TeeBuf::int_type
+TeeBuf::overflow(int_type c)
+{
+    for (auto buffer : buffers_)
+    {
+        if (traits_type::eof() == buffer->sputc(c))
+            return traits_type::eof();
+    }
+    return c;
+}
+
+std::streamsize
+TeeBuf::xsputn(const char_type* s, std::streamsize n)
+{
+    std::streamsize result{n};
+    for (auto buffer : buffers_)
+        result = std::min(result, buffer->sputn(s, n));
+    return result;
+}
+
+int
+TeeBuf::sync()
+{
+    int result{0};
+    for (auto buffer : buffers_)
+        result = std::min(buffer->pubsync(), result);
+    return result;
+}
+
+TeeBuf&
+TeeBuf::addBuffer(std::wstreambuf* buff)
+{
+    buffers_.push_back(buff);
+    return *this;
+}
+
+}
